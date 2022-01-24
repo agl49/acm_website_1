3fde27c933aa67ca02002ab73078ced4188586e0
agl49 <agl49@txstate.edu>
2022-01-23 21:19:18 -0600

    started work on personalCarousel component

---
+++ src/components/personalCarousel/pCarousel.js
@@ -0,0 +1,36 @@
+//A lot of the logic for this was extracted from the following:
+//https://github.com/sag1v/react-elastic-carousel
+//Package had too much logic for what we wanted so this is a simplified version.
+//Also, we convert the class componets used to functional components that use hooks as needed.
+import React, {Children} from "react";
+import PropTypes from "prop-types";
+import ResizeObserver from "resize-observer-polyfill";
+import Only from "react-only-when";
+
+//The lib we are reading changes the css based of the params via styled components. Is there
+//a way we can do this? Do we need to? Will it help making things reactive? 
+
+function PCarousel(props) {
+
+
+
+}
+
+export default PCarousel;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
